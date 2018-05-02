6a548f5511f61a6d5764b6b0d188fcea61bb5352
Louis Houghton <louis@louishoughton.com>
2018-05-02 09:53:11 +0100

    publish page change to websocket

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -9,12 +9,38 @@ import { PLATFORM_ID, APP_ID, Inject } from '@angular/core';
 import { isPlatformBrowser } from '@angular/common';
 import { AppRoutingModule } from './/app-routing.module';
 import { HomeComponent } from './home/home.component';
-import {EmViewerModule} from 'em-viewer-web';
+import { EmViewerModule } from 'em-viewer-web';
+import {StompConfig, StompService} from '@stomp/ng2-stompjs';
+import * as SockJS from "sockjs-client";
 
 const routes: Routes = [
     { path: '', component: HomeComponent }
 ];
 
+const stompConfig: StompConfig = {
+  // Which server?
+  // url: 'ws://127.0.0.1:15674/ws',
+  url: () => {return new SockJS('/icp/ws') as WebSocket;},
+
+  // Headers
+  // Typical keys: login, passcode, host
+  headers: {
+    login: 'guest',
+    passcode: 'guest'
+  },
+
+  // How often to heartbeat?
+  // Interval in milliseconds, set to 0 to disable
+  heartbeat_in: 0, // Typical value 0 - disabled
+  heartbeat_out: 20000, // Typical value 20000 - every 20 seconds
+  // Wait in milliseconds before attempting auto reconnect
+  // Set to 0 to disable
+  // Typical value 5000 (5 seconds)
+  reconnect_delay: 5000,
+
+  // Will log diagnostics on console
+  debug: true
+};
 
 @NgModule({
   declarations: [
@@ -29,7 +55,12 @@ const routes: Routes = [
       HttpClientModule,
       EmViewerModule
   ],
-  providers: [],
+  providers: [
+    StompService,
+    {
+      provide: StompConfig,
+      useValue: stompConfig
+    }],
   bootstrap: [AppComponent]
 })
 export class AppModule {
--- src/app/home/home.component.ts
+++ src/app/home/home.component.ts
@@ -1,5 +1,8 @@
 import {Component, OnInit} from '@angular/core';
-// import {MockServiceService} from '../mock-service.service';
+import {StompService} from '@stomp/ng2-stompjs';
+import {Message} from '@stomp/stompjs';
+import {Subscription} from 'rxjs/Subscription';
+import {Observable} from 'rxjs/Observable';
 
 @Component({
     selector: 'app-home',
@@ -8,20 +11,73 @@ import {Component, OnInit} from '@angular/core';
 })
 export class HomeComponent implements OnInit {
 
-    baseUrl = '/demproxy/dm/documents';
-    docId = 'be9f1344-d795-49a4-9946-cbf0acd74f9f';
-    url = `${this.baseUrl}/${this.docId}`;
+  baseUrl = '/demproxy/dm/documents';
+  docId = 'be9f1344-d795-49a4-9946-cbf0acd74f9f';
+  url = `${this.baseUrl}/${this.docId}`;
+  public page = 1;
 
-    constructor() {
+  public subscribed: boolean;
+  private subscription: Subscription;
+  private messages: Observable<Message>;
+
+  constructor(private stompService: StompService) {
+  }
+
+  onEnter(value: string) {
+    this.docId = value;
+    this.url = `${this.baseUrl}/${this.docId}`;
+  }
+
+  ngOnInit() {
+    this.subscribed = false;
+
+    // Store local reference to Observable
+    // for use with template ( | async )
+    this.subscribe();
+  }
+
+  public subscribe() {
+    if (this.subscribed) {
+      return;
     }
 
-    onEnter(value: string) {
-        this.docId = value;
-        this.url = `${this.baseUrl}/${this.docId}`;
+    // Stream of messages
+    this.messages = this.stompService.subscribe('/topic/screen-change/123');
+
+    // Subscribe a function to be run on_next message
+    this.subscription = this.messages.subscribe(this.onNext);
+
+    this.subscribed = true;
+  }
+
+  public unsubscribe() {
+    if (!this.subscribed) {
+      return;
     }
 
-    ngOnInit() {
-        // this.getData();
-    }
+    // This will internally unsubscribe from Stomp Broker
+    // There are two subscriptions - one created explicitly, the other created in the template by use of 'async'
+    this.subscription.unsubscribe();
+    this.subscription = null;
+    this.messages = null;
 
+    this.subscribed = false;
+  }
+
+  ngOnDestroy() {
+    this.unsubscribe();
+  }
+
+  public pageChange(page: number) {
+    this.page = page;
+    this.stompService.publish('/icp/screen-change/123',
+      `{"page":  ${page}}`);
+  }
+
+  onNext = (message: Message) => {
+    // Log it to the console
+    console.log(message);
+    const update = JSON.parse(message.body);
+    this.page = update.page;
+  }
 }
