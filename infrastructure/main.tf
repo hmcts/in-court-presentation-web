ddd4df3b48ac509fdc0058113835a375500f5f0b
Louis Houghton <louis@louishoughton.com>
2018-05-03 15:09:28 +0100

    Test moj-module-webapp websocket change

--- infrastructure/main.tf
+++ infrastructure/main.tf
@@ -7,7 +7,7 @@ locals {
 # "${local.app_full_name}"
 
 module "app" {
-  source = "git@github.com:hmcts/moj-module-webapp?ref=master"
+  source = "git@github.com:hmcts/moj-module-webapp?ref=Louisblack-patch-1 "
   product = "${local.app_full_name}"
   location = "${var.location}"
   env = "${var.env}"
@@ -17,6 +17,7 @@ module "app" {
   is_frontend = true
   additional_host_name = "${local.app_full_name}-${var.env}.service.${var.env}.platform.hmcts.net"
   https_only="true"
+  webSocketsEnabled="true"
 
   app_settings = {
     # REDIS_HOST = "${module.redis-cache.host_name}"
