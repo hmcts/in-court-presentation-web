00a69d5a6695fb1b7b7dbd1dc375c0f8e9be6b2d
Louis Houghton <louis@louishoughton.com>
2018-05-04 13:10:02 +0100

    Test removing websockets parameter

--- infrastructure/main.tf
+++ infrastructure/main.tf
@@ -17,7 +17,6 @@ module "app" {
   is_frontend = true
   additional_host_name = "${local.app_full_name}-${var.env}.service.${var.env}.platform.hmcts.net"
   https_only="true"
-  web_sockets_enabled="true"
 
   app_settings = {
     # REDIS_HOST = "${module.redis-cache.host_name}"
