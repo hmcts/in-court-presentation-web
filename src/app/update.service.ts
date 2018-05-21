92fd81dffc32ddeb535e2912a7ac3f1ce4c24a87
Louis Houghton <louis@louishoughton.com>
2018-05-21 14:49:59 +0100

    Remove unused config

--- src/app/update.service.ts
+++ src/app/update.service.ts
@@ -1,37 +1,9 @@
 import {HostListener, Injectable} from '@angular/core';
-import {StompConfig, StompService} from '@stomp/ng2-stompjs';
+import {StompService} from '@stomp/ng2-stompjs';
 import {Observable} from 'rxjs/Observable';
 import {Subscription} from 'rxjs/Subscription';
 import {Message} from '@stomp/stompjs';
-import * as SockJS from 'sockjs-client';
 import {StompServiceFactoryService} from './stomp-service-factory.service';
-
-const stompConfig: StompConfig = {
-  // Which server?
-  // url: 'ws://127.0.0.1:15674/ws',
-  url: () => {
-    return new SockJS('/icp/ws') as WebSocket;
-  },
-
-  // Headers
-  // Typical keys: login, passcode, host
-  headers: {
-    login: 'guest',
-    passcode: 'guest',
-  },
-
-  // How often to heartbeat?
-  // Interval in milliseconds, set to 0 to disable
-  heartbeat_in: 0, // Typical value 0 - disabled
-  heartbeat_out: 20000, // Typical value 20000 - every 20 seconds
-  // Wait in milliseconds before attempting auto reconnect
-  // Set to 0 to disable
-  // Typical value 5000 (5 seconds)
-  reconnect_delay: 5000,
-
-  // Will log diagnostics on console
-  debug: true
-};
 
 @Injectable()
 export class UpdateService {
